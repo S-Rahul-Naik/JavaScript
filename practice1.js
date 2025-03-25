ae2caaeef136f41ba475126d293ad68526496058
rahul235004 <23rahul54@gmail.com>
2025-03-25 20:11:01 +0530

    practiced conditional statements

---
+++ practice1.js
@@ -0,0 +1,117 @@
+// Multiple if condition vthout asking user to enter input
+
+// console.log("before if else condi.");
+// let age = 20;
+// if (age>=18)
+//     {
+//     console.log("You are eligible to vote.");
+//     let a=5;
+//     console.log(5*a);
+//     }
+// if (age<18){
+//     console.log("cant vote")
+// }
+// console.log("after if else condi.");
+
+
+
+
+
+
+
+// Traffic light sysytem for beginners
+
+// let color = "green";
+// if (color ==="red"){
+//     console.log(`stop, color is ${color}`);
+// }
+// else if (color ==="yellow"){
+//     console.log(`slow down, color is ${color}`);
+// }
+// else if (color ==="green"){
+//     console.log(`go, color is ${color}`);
+// }
+
+
+
+
+
+
+// else if conditinal statements
+
+// let agie = 18;
+// if (agie>=18) {
+//     console.log("you can vote");
+// } 
+// else if(agie < 18) {
+//     console.log("you cant vote");
+// } 
+
+
+
+
+
+// marks
+
+// let marks =75;
+// if (marks >=80){
+//     console.log("A+");
+// } else if (marks >=60){
+//     console.log("A");
+// }else if (marks>=33){
+//     console.log("B");
+// }else if (marks<33){
+//     console.log("F");
+// }
+
+
+// seasons
+
+// let month ='april'
+// if(month==="jan"){
+//     console.log("winter is here");
+// }else if (month==="april"){
+//     console.log("summer is here")
+// }
+
+
+
+
+
+// // else conditinal statements
+
+// let agie = 17;
+// if (agie>=18) {
+//     console.log("you can vote");
+// } 
+// else {
+//     console.log("you cant vote");
+// }
+
+
+
+// Traffic light sysytem for beginners
+
+// let color = "greens";
+// if (color ==="red"){
+//     console.log(`stop, color is ${color}`);
+// }
+// else if (color ==="yellow"){
+//     console.log(`slow down, color is ${color}`);
+// }
+// else if (color ==="green"){
+//     console.log(`go, color is ${color}`);
+// }
+// else {
+//     console.log("traffic lights are broken");
+// }
+
+
+
+
+
+
+
+
+
+// 
